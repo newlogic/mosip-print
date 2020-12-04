f46f89884a3bc35703c12417f2e99c89f5b07d6f
Dexter Aparicio <dexter@newlogic.io>
2020-12-04 22:25:14 +0800

    Branch 1.1.3 uses 8088 listening port

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
 
-mvn -Dspring.profiles.active=local -Dmosip.event.hubURL=http://13.232.228.161:9191/websub -Dmosip.event.callBackUrl="http://188.166.234.246:8099/print/callback/notifyPrint" -Dmosip.primary-language=eng -Dmosip.secondary-language=fra -Dmosip.datashare.partner.id=newlogic -Dmosip.datashare.policy.id=newlogic spring-boot:run
+mvn -Dspring.profiles.active=local -Dmosip.event.hubURL=http://13.232.228.161:9191/websub -Dmosip.event.callBackUrl="http://188.166.234.246:8088/print/callback/notifyPrint" -Dmosip.primary-language=eng -Dmosip.secondary-language=fra -Dmosip.datashare.partner.id=newlogic -Dmosip.datashare.policy.id=newlogic spring-boot:run
 
 
